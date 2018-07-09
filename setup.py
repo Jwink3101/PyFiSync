ed05e5bce149355fd5ffac868cbef7d1b96bf154
Jwink3101 <Jwink3101@users.noreply.github.com>
2018-07-09 07:46:30 -0600

    Fix setup.py for case sensitive machines Copy from local devel repo: [git: PyFiSync.git/] 808af0731a: (2018-07-09 07:46:28 -0600)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 setup(
     name='PyFiSync',
     packages=['PyFiSync'],
-    long_description=open('readme.md').read(),
+    long_description=open('README.md').read(),
     entry_points = {
         'console_scripts': ['PyFiSync=PyFiSync.main:cli'],
     },
